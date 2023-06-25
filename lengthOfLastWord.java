c5d0216b02152cfc5c8a392b4509eecf9369f8a3
Ibraheem Qadi <ibra444hem@gmail.com>
2023-06-25 08:49:34 +0300

    Length of Last Word

---
+++ lengthOfLastWord.java
@@ -0,0 +1,11 @@
+// 58. Length of Last Word
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int c = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) != ' ') c++;
+            else if (c != 0) break;
+        }
+        return c;
+    }
+}
