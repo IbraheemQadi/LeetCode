b01ee71bb8adb845f9376657ca93daf8d6e8ba8f
Ibraheem Qadi <ibra444hem@gmail.com>
2023-06-29 12:44:22 +0300

    Is Subsequence.

---
+++ LeetCode 75/isSubsequence.java
@@ -0,0 +1,17 @@
+// 392. Is Subsequence
+public boolean isSubsequence(String s, String t) {
+       int i = 0, j = 0;
+        
+        while (i < s.length() && j < t.length()) {
+            if (s.charAt(i) == t.charAt(j)) {
+                i += 1;
+            }
+            j += 1;
+        }
+    
+        if (i == s.length()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
